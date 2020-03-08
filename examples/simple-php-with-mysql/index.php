82292e12d5ccbd0cc5435c47c30bc5f006dc52b8
Khaled Alam <khaledalam.net@gmail.com>
2020-03-08 20:00:39 +0000

    update test php example

--- examples/simple-php-with-mysql/index.php
+++ examples/simple-php-with-mysql/index.php
@@ -1,7 +1,21 @@
 <?php
 
+echo "<h3>Testing...</h3>";
+
 echo "print text in php... 100%" . "<hr>";
 
-$link = mysqli_connect("localhost","root","","khaledalamDBNAME") or die("Can't Connect... 0%");
+$link = mysqli_connect("localhost", "root", "", "khaledalamDBNAME") or die("Can't Connect... 0%");
 
-echo "MySQL connect successfully... 100%" . "<br>";
+echo "MySQL connect successfully... 100%" . "<hr><br>";
+
+
+echo "Data from DB:" . "<br>";
+
+$r = mysqli_query($link, "SELECT * from users");
+
+while($row = $r->fetch_assoc())
+{
+        var_dump($row);
+	echo " 100%<hr>";
+}
+
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 
 APP_NAME="awesome-docker-lamp-php-example"
 
-PORT_HOST=8001
+PORT_HOST=8000
 PORT_INNER=80
 
 PORT_HOST_SQL=3306
@@ -17,41 +17,42 @@ DB_NAME="khaledalamDBNAME";
 DB_SQL_FILE=examples/simple-php-with-mysql/dummy.sql;
 
 
-
-echo "Start Cloning...";
+echo "Start cloning...";
 sudo rm -rf ${APP_NAME} || true;
 git clone ${REPO} ${APP_NAME};
-echo "Finish Cloning.";
+echo "Finish cloning.";
 
 
-echo "Start Dockering...";
+echo "Start dockering...";
 docker rm ${APP_NAME} --force || true;
 docker run -d \
-    -p ${PORT_HOST}:${PORT_INNER} \
+	-p ${PORT_HOST}:${PORT_INNER} \
 	-p ${PORT_HOST_SQL}:${PORT_INNER_SQL} \
-    -v $PWD/examples/simple-php-with-mysql:/app \
-    --name ${APP_NAME} mattrayner/lamp:latest-1804;
-echo "Finish Dockering.";
+	-v $PWD/examples/simple-php-with-mysql:/app \
+	--name ${APP_NAME} mattrayner/lamp:latest-1804;
+echo "Finish dockering.";
 
-# sudo chmod 777 /var/run/mysqld/mysqld.sock
 
+echo "Start waiting for confirmation of mysql service";
 RET=1
 while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup"
-    sleep 5
-    mysql -uroot -e "status" > /dev/null 2>&1
-    RET=$?
+    echo "=> Waiting for confirmation of MySQL service startup";
+    sleep 10;
+    mysql -uroot -e "status" > /dev/null 2>&1;
+    RET=$?;
 done
-
+echo "Finish waiting."
+# sleep 10
 
 
 echo "Start Create Database Process...";
 docker exec -i ${APP_NAME} mysql -uroot -e "create database ${DB_NAME};";
-echo "Start Create Database Process...";
+echo "Finish create Database.";
 
-echo "Start Insert dummy data in DB...";
+
+echo "Start insert dummy data in DB...";
 docker exec -i ${APP_NAME} mysql ${DB_NAME} < ${DB_SQL_FILE};
-echo "Finish Insertion.";
+echo "Finish insertion.";
 
 
 echo "Done! :)";
