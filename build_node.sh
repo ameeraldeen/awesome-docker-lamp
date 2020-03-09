cc95371144d1fd6b111216677168a8668c1d1585
Khaled Alam <khaledalam.net@gmail.com>
2020-03-09 13:38:51 +0000

    fix node script and example

--- build_node.sh
+++ build_node.sh
@@ -5,8 +5,8 @@
 
 APP_NAME="awesome-docker-lamp-node-example"
 
-PORT_HOST=8000
-PORT_INNER=80
+PORT_HOST=8002
+PORT_INNER=8002
 
 PORT_HOST_SQL=3306
 PORT_INNER_SQL=3306
@@ -27,36 +27,36 @@ echo "Start dockering...";
 docker rm ${APP_NAME} --force || true;
 docker run -d \
 	-p ${PORT_HOST}:${PORT_INNER} \
-	-p ${PORT_HOST_SQL}:${PORT_INNER_SQL} \
 	-v $PWD/examples/simple-node-app:/app \
 	--name ${APP_NAME} khaledalam/awesome-docker-lamp:latest-with-node;
 echo "Finish dockering.";
 
 
 
-<<COMMENT1
-echo "Start waiting for confirmation of mysql service";
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup";
-    sleep 10;
-    mysql -uroot -e "status" > /dev/null 2>&1;
-    RET=$?;
-done
-echo "Finish waiting."
-# sleep 10
-COMMENT1
+
+#echo "Start waiting for confirmation of mysql service";
+#RET=1
+#while [[ RET -ne 0 ]]; do
+#    echo "=> Waiting for confirmation of MySQL service startup";
+#    sleep 10;
+#    mysql -uroot -e "status" > /dev/null 2>&1;
+#    RET=$?;
+#done
+#echo "Finish waiting."
+#sleep 10
 
 
-echo "Start Create Database Process...";
-docker exec -i ${APP_NAME} mysql -uroot -e "create database ${DB_NAME};";
-echo "Finish create Database.";
+# echo "Start Create Database Process...";
+# docker exec -i ${APP_NAME} mysql -uroot -e "create database ${DB_NAME};";
+# echo "Finish create Database.";
 
 
-echo "Start insert dummy data in DB...";
-docker exec -i ${APP_NAME} mysql ${DB_NAME} < ${DB_SQL_FILE};
-echo "Finish insertion.";
+#echo "Start insert dummy data in DB...";
+#docker exec -i ${APP_NAME} mysql ${DB_NAME} < ${DB_SQL_FILE};
+#echo "Finish insertion.";
 
+echo "Starting app...";
+docker exec -i ${APP_NAME} bash -c "cd app; npm install express; npm install nodemon; npm run start;"
 
 echo "Done! :)";
 
